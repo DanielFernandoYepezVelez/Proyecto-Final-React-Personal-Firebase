f5e6ec0383db1c58d43ea1014a5d83250f8f1916
405 <405@academicos.cesde.edu.co>
2022-12-13 11:29:49 -0500

    Agregando La Descripcion De Los Productos Y El Disenio

--- src/Components/InformationProduct/InformationProduct.js
+++ src/Components/InformationProduct/InformationProduct.js
@@ -1,14 +1,54 @@
 import { useLocation } from 'react-router-dom'
 
+import './InformationProduct.css';
+
 export function InformationProduct() {
     let getData = useLocation();
     let product = getData['state']['selectedProduct'];
 
-    console.log(product);
-
     return(
         <>
-            <h1>{product['information']['name']}</h1>
+            <div className='container'>
+                <div className='row m-5'>
+                    <div className='col-12 col-md-6'>
+                        <div className='box__container--areaMain'>
+                            <div className='box__one1INFO--main'></div>
+                            <div className='box__two1INFO--main'></div>
+                        </div>
+                        {/* <img className='img-fluid w-100' src={product['information']['photo']} alt='imageProduct'/> */}
+                    </div>
+                    <div className='col-12 col-md-4 borderProduct p-4'>
+                        <h2 className='fw-bold text-center'> {product['information']['name']} </h2>
+                        <h1 className='mt-3 display-1'>${product['information']['price']} <span className='displa-4 discount'>20% off</span></h1>
+                        
+                        <p>{product['information']['quantity']} Unidades</p>
+                        <p>Disponible: {product['information']['available']} </p>
+                        
+                        <span className='badge text-bg-primary'>Descuento Del Dia</span>
+                        <p>Hasta 48 Cuotas</p>
+                        <img src='https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/visa.png?alt=media&token=08b4ad6a-65ea-452f-9c4c-84d38efeacf3' alt='logoVisa'/>
+                        <img className='ms-3' src='https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/american-express.png?alt=media&token=f84efa31-d1a0-4650-a974-17cbbae41f38' alt='logoAmericanExpress'/>
+                        
+                        <br />
+                        <div className='d-flex align-items-center'>
+                            <img className='me-2' src='https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/track.png?alt=media&token=2d25a698-1563-4b0f-ac3c-ad1633d20611' alt='logoTrack'/>
+                            <p className='text-success fw-bold'>Envio Gratis A Nivel Nacional</p>
+                        </div>
+                        <p>Conoce Tiempos Y Formas De Envio.</p>
+
+                        <div className='d-flex align-items-center'>
+                            <img className='me-2' src='https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/exchange.png?alt=media&token=126be718-c439-4a5a-a310-c9c7fb6a4dda' alt='logoTrack'/>
+                            <p className='text-success fw-bold'>Devolución Gratis</p>
+                        </div>
+                        <p>30 Días Para Reembolso.</p>
+                        <div>
+                            <p>{product['information']['description']}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* <h1>{product['information']['name']}</h1> */}
         </>
     )
 }
--- src/Components/Merchandise/Merchandise.js
+++ src/Components/Merchandise/Merchandise.js
@@ -24,7 +24,8 @@ export function Merchandise() {
                 photo: "https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/productOne.jpg?alt=media&token=f3896047-a675-4515-a112-4f8caf443ad8",
                 price: 39.99,
                 available: "si",
-                quantity: 10
+                quantity: 10,
+                description: "Camiseta Original De La Banda Description",
             }
         },
         {
@@ -37,7 +38,8 @@ export function Merchandise() {
                 photo: "https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/productThree.jpg?alt=media&token=593b1f68-0a2f-44c5-9b58-9facf1591971",
                 price: 59.99,
                 available: "si",
-                quantity: 30
+                quantity: 30,
+                description: "Video Concierto One Direction Tour Description",
             }
         },
         {
@@ -50,7 +52,8 @@ export function Merchandise() {
                 photo: "https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/productFour.jpg?alt=media&token=6b573806-5db1-4f58-bca0-5dcc79929bb8",
                 price: 9.99,
                 available: "si",
-                quantity: 40
+                quantity: 40,
+                description: "Cartuchera Estudiantil Description",
             }
         },
         {
@@ -59,11 +62,12 @@ export function Merchandise() {
             nameBoxTwo: "box__two4--main",
             nameBoxThreeText: "box__container--textPosition2",
             information: {
-                name: "Permufe Sauvage One Direction",
+                name: "Perfume Sauvage One Direction",
                 photo: "https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/productFive.jpg?alt=media&token=3ff8459e-26b6-4742-8188-519446a6612a",
                 price: 9.99,
                 available: "si",
-                quantity: 40
+                quantity: 40,
+                description: "Perfume Sauvage One Direction Description",
             }
         },
         {
@@ -76,7 +80,8 @@ export function Merchandise() {
                 photo: "https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/productSix.jpg?alt=media&token=92d184b2-0638-41a3-ae56-e0ae76be57be",
                 price: 29.99,
                 available: "si",
-                quantity: 50
+                quantity: 50,
+                description: "Chompa Negra XL Description",
             }
         },
         {
@@ -89,7 +94,8 @@ export function Merchandise() {
                 photo: "https://firebasestorage.googleapis.com/v0/b/reactpersonalspotifyapp.appspot.com/o/productSix.jpg?alt=media&token=92d184b2-0638-41a3-ae56-e0ae76be57be",
                 price: 79.99,
                 available: "si",
-                quantity: 60
+                quantity: 60,
+                description: "Vinilo De La Banda One Direction Description",
             }
         }
     ];
